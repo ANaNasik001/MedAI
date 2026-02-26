c05c9b6c438525e19de6a6e946c7522d5a5f2274
Ananas <ananas123@gmail.com>
2026-02-27 00:31:22 +0500

    Fix settings for Cloudinary

--- medai_project/settings.py
+++ medai_project/settings.py
@@ -131,12 +131,10 @@ if not DEBUG:
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
-    import os
-import cloudinary
 
-cloudinary.config(
-    cloud_name = os.environ.get("CLOUDINARY_CLOUD_NAME"),
-    api_key = os.environ.get("CLOUDINARY_API_KEY"),
-    api_secret = os.environ.get("CLOUDINARY_API_SECRET")
-)
+INSTALLED_APPS += [
+    "cloudinary",
+    "cloudinary_storage",
+]
 
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
